b46a269badbfbdccb93e4b12416edeba5289203f
David <badadavid04@gmail.com>
2022-03-22 11:47:03 +0100

    histom2.0

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+const get = document.querySelector('#get');
+const popup = document.querySelector('.popup')
+const closeBtn = document.querySelector('#close-btn')
+
+closeBtn.addEventListener('click',(event)=>{
+  popup.style.display = 'none !important'
+})
+
